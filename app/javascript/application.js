f85c745efec20e1c5be071159d1fe8633c9d4bec
Bhekisile <bhekisilejozi@gmail.com>
2024-08-03 11:14:17 +0200

    Setup d3 properly

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
 
 //= require jquery
+//= require jquery-ui/datepicker
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require jquery-ui/datepicker
 //= require d3
 // require turbolinks
+//= require exercise
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,8 +2,11 @@
 
 # Pin npm packages by running ./bin/importmap
 
-pin 'application'
-pin '@hotwired/turbo-rails', to: 'turbo.min.js'
-pin '@hotwired/stimulus', to: 'stimulus.min.js'
-pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
-pin_all_from 'app/javascript/controllers', under: 'controllers'
+# pin 'application', preload: true
+pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
+pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
+pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
+pin 'jquery', to: 'jquery.min.js', preload: true
+pin 'jquery_ujs', to: 'jquery_ujs.min.js', preload: true
+pin 'd3', to: 'https://cdn.jsdelivr.net/npm/d3@7'
+# pin 'popper', to: 'popper.js', preload: true
